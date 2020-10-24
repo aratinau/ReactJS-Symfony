fc5bb65bea5ae11780e8629a212e93744b2c0de4
Aymeric <hello@codeparadise.xyz>
2020-10-24 17:44:45 +0200

    CSRF Protection

--- assets/js/api/rep_log_api.js
+++ assets/js/api/rep_log_api.js
@@ -1,6 +1,15 @@
 function fetchJson(url, options) {
+    let headers = {'Content-Type': 'application/json'};
+
+    if (options && options.headers) {
+        headers = Object.assign({}, options.headers, headers);
+        
+        delete options.headers;
+    }
+
     return fetch(url, Object.assign({
         credentials: 'same-origin',
+        headers: headers,
     }, options))
         .then(checkStatus)
         .then(response => {
@@ -41,8 +50,5 @@ export function createRepLog(repLog) {
     return fetchJson('/reps', {
         method: 'POST',
         body: JSON.stringify(repLog),
-        headers: {
-            'Content-Type': 'application/json'
-        }
     })
 }
--- src/Controller/RepLogController.php
+++ src/Controller/RepLogController.php
@@ -10,9 +10,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use App\Api\ApiRoute;
 
 /**
  * @IsGranted("ROLE_USER")
+ * @ApiRoute(defaults={"_is_api": true})
  */
 class RepLogController extends BaseController
 {
