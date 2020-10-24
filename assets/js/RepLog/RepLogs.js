e5c78fb68600195afc1aec3dcaa7305b872767b6
Aymeric <hello@codeparadise.xyz>
2020-10-24 09:54:08 +0200

    Controlled Form Input

--- assets/js/RepLog/RepLogs.js
+++ assets/js/RepLog/RepLogs.js
@@ -21,17 +21,27 @@ export default function RepLogs(props) {
         highlightedRowId,
         onRowClick,
         repLogs,
-        onAddRepLog
+        onAddRepLog,
+        numberOfHearts,
+        onHeartChange
     } = props;
 
     let heart = '';
     if (withHeart) {
-        heart = <span>❤️</span>;
+        heart = <span>{'❤️'.repeat(numberOfHearts)}️</span>;
     }
 
     return (
         <div className="col-md-7">
             <h2>Lift Stuff! {heart}</h2>
+
+            <input
+                type="range"
+                value={numberOfHearts}
+                onChange={(e) => {
+                    onHeartChange(+e.target.value) // le + converti la string en number
+                }}
+            />
 
             <table className="table table-striped">
                 <thead>
@@ -73,5 +83,7 @@ RepLogs.propTypes = {
     highlightedRowId: PropTypes.any,
     onRowClick: PropTypes.func.isRequired,
     onAddRepLog: PropTypes.func.isRequired,
-    repLogs: PropTypes.array.isRequired
+    onHeartChange: PropTypes.func.isRequired,
+    repLogs: PropTypes.array.isRequired,
+    numberOfHearts: PropTypes.number.isRequired
 };
